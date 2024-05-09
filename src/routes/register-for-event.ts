4a8d427f1fdd0987a0f75124b8ed2dc1263175fb
EsdrasCFF <ecff.dev@gmail.com>
2024-05-09 09:44:39 -0300

    feat: create new api route to register for event

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function regiserForEvent(app:FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      '/events/:eventId/attendees',
+      
+      {schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email()
+        }),
+        params: z.object({
+          eventId: z.string().uuid()
+        }),
+        response: {
+          201: z.object({
+            attendeeId: z.number()
+          })
+        }
+      }}, 
+      
+      async (request, response) => {
+        const { name, email } = request.body
+        const { eventId } = request.params
+      
+        const attendee = await prisma.attendee.create({
+          data: {
+            name,
+            email,
+            eventId,
+          }
+        })
+
+        return response.code(201).send({attendeeId: attendee.id})
+      }
+    )
+}
