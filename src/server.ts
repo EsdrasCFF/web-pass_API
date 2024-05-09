59f8b8c6729f7356cd9b44a8d6512dfd85d6b4e8
EsdrasCFF <ecff.dev@gmail.com>
2024-05-08 23:41:04 -0300

    chore: add zod to handle erros

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,35 @@
 import fastify from "fastify"
+import z from "zod"
+import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+
+const prisma = new PrismaClient({
+  log: ['query', 'error']
+})
 
 const app = fastify()
+
+app.post('/events', async (request, response) => {
+  
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable()
+  })
+
+  const data = createEventSchema.parse(request.body)
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      slug: faker.company.name(),
+      maximumAttendees: data.maximumAttendees,
+      details: data.details
+    }
+  })
+
+  return response.code(201).send({eventId: event.id})
+})
 
 app.listen({port: 3333}).then(() => {
   console.log('Server is running')
