d6be74445b03c463a4955654a877e4022e12498e
EsdrasCFF <ecff.dev@gmail.com>
2024-05-12 00:19:28 -0300

    feat: add fastify swagger to write api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import fastify from "fastify"
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -7,10 +7,30 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/getEventsAttendees";
 
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'web-pass',
+      description: 'Especificações da API Web-Pass, backend da aplicação Web-Pass, construida para gerenciar eventos e participantes',
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs'
+})
 
 app.register(createEvent)
 app.register(registerForEvent)
